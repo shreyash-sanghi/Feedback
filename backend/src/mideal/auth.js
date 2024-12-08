6c84f7a7d096e1c9e4f713c3f756e075dd10ef6d
Shreyash Jain <148464006+shreyash-sanghi@users.noreply.github.com>
2024-12-08 15:08:30 +0530

    Update auth.js

--- backend/src/mideal/auth.js
+++ backend/src/mideal/auth.js
@@ -3,7 +3,6 @@ const jwt = require("jsonwebtoken");
 const addData = async (req,res,next)=>{
     try {
         const token = req.header('Authorization');
-console.log("token",token)
         const varifyUser = jwt.verify(token,process.env.SecretKey);
 
         req.Token = token;
